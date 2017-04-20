a0844dbf7b49147335faea9ba4122a6b608d8f18
151250063 <151250063@smail.nju.edu.cn>
2017-04-20 11:34:06 +0900

    实现拖动

--- src/Mafuyo.java
+++ src/Mafuyo.java
@@ -1,6 +1,8 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -36,6 +38,9 @@ public class Mafuyo extends JFrame{
         this.setSize(getWidth(path), getHeight(path));
         this.setUndecorated(true);
         this.setBackground(new Color(0, 0, 0, 0));
+        MouseEventListener mouseListener = new MouseEventListener(this);
+        this.addMouseListener(mouseListener);
+        this.addMouseMotionListener(mouseListener);
         this.setVisible(true);
     }
 
@@ -46,6 +51,65 @@ public class Mafuyo extends JFrame{
         String path="src/imgs/mafuyo.png";
         ImageIcon ii1 = new ImageIcon(path);
         g2.drawImage(ii1.getImage(), 0, 0, getWidth(path),getHeight(path),null);
+    }
+
+    class MouseEventListener implements MouseInputListener {
+
+        Point origin;
+        //鼠标拖拽想要移动的目标组件
+        Mafuyo frame;
+
+        public MouseEventListener(Mafuyo frame) {
+            this.frame = frame;
+            origin = new Point();
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {}
+
+        /**
+         * 记录鼠标按下时的点
+         */
+        @Override
+        public void mousePressed(MouseEvent e) {
+            origin.x = e.getX();
+            origin.y = e.getY();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {}
+
+        /**
+         * 鼠标移进标题栏时，设置鼠标图标为移动图标
+         */
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            this.frame.setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
+        }
+
+        /**
+         * 鼠标移出标题栏时，设置鼠标图标为默认指针
+         */
+        @Override
+        public void mouseExited(MouseEvent e) {
+            this.frame.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        }
+
+        /**
+         * 鼠标在标题栏拖拽时，设置窗口的坐标位置
+         * 窗口新的坐标位置 = 移动前坐标位置+（鼠标指针当前坐标-鼠标按下时指针的位置）
+         */
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            Point p = this.frame.getLocation();
+            this.frame.setLocation(
+                    p.x + (e.getX() - origin.x),
+                    p.y + (e.getY() - origin.y));
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {}
+
     }
 
     public static void main(String[] args) {
